e17849ddc8bb2e44c2783d3c9865f9d334967cb1
BohdanZ13 <bza@ethinking.de>
2018-06-02 22:58:40 +0200

    Add files via upload

---
+++ question.js
@@ -0,0 +1,296 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_DE_DE: [
+        {
+            'Welche Sportwagenmanufaktur ist im italienischen Maranello beheimatet?': [
+                'testTimeFerrari',
+                'Lamborghini',
+                'B-Engineering',
+            ],
+        },
+        {
+            'Wie viele Zylinder hat die Dodge Viper? ': [
+                '10',
+                '6',
+                '4',
+            ],
+        },
+        {
+            'Was entwickelte BMW urspünglich?': [
+                'Flugzeugmotoren',
+                'Nähmaschinen',
+                'Schiffsmotoren',
+            ],
+        },
+        {
+            'Welche dieser Automarken kommt nicht aus Korea?': [
+                'Subaru',
+                'Kia',
+                'Hyundai',
+            ],
+        },
+        {
+            'Von welchem Autopionier stammt der Satz.  Wer nicht wirbt, stirbt!': [
+                'Henry Ford',
+                'Carl Benz',
+                'Ferdinand Porsche',
+            ],
+        },
+        {
+            'Welche beiden Automarken gehören nicht zum selben Konzern?': [
+                'Volkswagen und Mazda',
+                'Fiat und Ferrari',
+                'Renault und Dacia',
+            ],
+        },
+        {
+            'Was ist die "Thin Lizzy"?': [
+                'Das erste Modell von Ford',
+                'Ein Rennauto von Bentley',
+                'Ein früher Zweisitzer von Carl Benz',
+            ],
+        },
+        {
+            'Woher stammte Ferdiand Porsche?': [
+                'Böhmen',
+                'Südtirol',
+                'Deutschland',
+            ],
+        },
+        {
+            'Welcher James Bond Darsteller fuhr im Film keinen Aston Martin?': [
+                'Roger Moore',
+                'Pierce Brosnam',
+                'Sean Connery',
+            ],
+        },
+        {
+            'Welchen Wagen ziert eine weibliche Kühlerfigur, die "Spirit of Ecstasy" genannt wird?': [
+                ' Rolls-Royce',
+                'Chevrolet',
+                'Lamborghini',
+            ],
+        },
+        {
+            'Welcher asiatische Autohersteller entwickelte den humanoiden Roboter Asimo?': [
+                'Honda',
+                'Mitsubishi',
+                'Hyundai',
+            ],
+        },
+        {
+            'In welchem Auto fuhr Dustin Hoffman im Filmklassiker die Reifeprüfung zu Mrs. Robinson?': [
+                'Alfa Romeo Spider',
+                'VW "Bulli"',
+                'Ford Mustang',
+            ],
+        },
+        {
+            'Wer produziert das kleinste Automobll?': [
+                'Tata - (Nano)',
+                'BMW - (Mini)',
+                'Daimler - (Smart)',
+            ],
+        },
+        {
+            'Wo ist der traditionelle Firmensitz von Bugatti?': [
+                'Elsass. (Frankreich)',
+                'Lombardien. (Italien)',
+                'Liechtenstein',
+            ],
+        },
+        {
+            'In welchem Jahr wurde der erste Strafzettel ausgestellt?': [
+                '1902',
+                '1915',
+                '1929',
+            ],
+        },
+        {
+            'Wer war zeitweise der größte Fahrradhersteller der Welt?': [
+                'Opel',
+                'Suzuki',
+                'Peugeot',
+            ],
+        },
+        {
+            'Wann wurde das Auto erfunden?': [
+                '1886',
+                '1923',
+                '1902',
+            ],
+        },
+        {
+            'Was ist ein Kei-Car?': [
+                ' Japanische Kleinwagenklasse',
+                'Ein Rennwagen mit Raketenantrieb',
+                'Ein Auto mit Pedalantrieb',
+            ],
+        },
+        {
+            'Was bedeutet der Begriff Karosserie der Wortherkunft nach?': [
+                ' Kutsche',
+                'Käfig',
+                'Karre',
+            ],
+        },
+        {
+            'Wer baute den ersten sogenannten Motorwagen?': [
+                ' Carl Benz',
+                'Gottlieb Daimler',
+                'Henry Ford',
+            ],
+        },
+        {
+            'Wann wurde das erste bekannte deutsche Elektroauto gebaut?': [
+                ' 1888',
+                '1936',
+                '1967',
+            ],
+        },
+        {
+            'Welches Auto verkaufte sich bis heute weltweit am häufigsten?': [
+                'Toyota Corolla',
+                'Volkswagen Golf',
+                'Honda Civic',
+            ],
+        },
+        {
+            'Was versteht man unter einem Hybridauto?': [
+                ' Ein Auto, das wahlweise mit Verbrennungs- oder Elektromotor angetrieben werden kann.',
+                'Ein Auto, das speziell für felsiges Gelände gebaut wurde.',
+                'Ein Auto, das an Land und auf dem Wasser fahren kann.',
+            ],
+        },
+        {
+            'Wer ist nach Michael Schumacher der beste Formel-1-Fahrer aller Zeiten?': [
+                ' Juan Manuel Fangio',
+                '//',
+                '//',
+            ],
+        },
+        {
+            'Wie viele Autos sind in Deutschland derzeit in etwa zugelassen?': [
+                ' 43.4 Millionen',
+                '80 Millionen',
+                '434.000',
+            ],
+        },
+        {
+            'Wie hieß die ursprüngliche Automarke des Audi-Gründers?': [
+                'Horch',
+                'Sieh',
+                'Sprich',
+            ],
+        },
+        {
+            'Unter welcher Voraussetzung darf man in Deutschland bereits mit 17 Jahren Auto fahren? ': [
+                'Man muss sich bei der Fahrt begleiten lassen.',
+                'Man muss einen psychologischen Test bestehen.',
+                'Man muss die Fahrprüfung mit Auszeichnung bestehen.',
+            ],
+        },
+        {
+            'Welche dieser Aussagen trifft nicht zu? ': [
+                'Am autofreien Sonntag ist es generell verboten, Auto zu fahren.',
+                'Auf der Insel Helgoland herrscht ein generelles Auto-Verbot.',
+                'Wenn das Auto zu viel Feinstaub produziert, darf man in manchen Stadtteilen nicht damit fahren.',
+            ],
+        },
+        // {
+        //     'Wie hat der Vater von Rudolph, Donner, im Film aus dem Jahr 1964 versucht, die Nase von Rudolph zu verbergen?': [
+        //         'Schwarzer Schlamm',
+        //         'Sack',
+        //         'Kissenbezug',
+        //         'Socke',
+        //     ],
+        // },
+        // {
+        //     'Was möchte die Misfit-Elfe im Film aus dem Jahr 1964 werden anstatt eine Elfe für den Weihnachtsmann?': [
+        //         'Zahnarzt',
+        //         'Rentier',
+        //         'Spielzeugmacher',
+        //         'Kerzenmacher',
+        //     ],
+        // },
+        // {
+        //     'Was war die einzige Schwäche von Bumble im Film aus dem Jahr 1964?': [
+        //         'Konnte nicht schwimmen',
+        //         'War immer hungrig',
+        //         'Zuckerstangen',
+        //         'Schielte',
+        //     ],
+        // },
+        // {
+        //     'Was sucht Yukon Cornelius in Wirklichkeit im Film aus dem Jahr 1964?': [
+        //         'Pfefferminz',
+        //         'Gold',
+        //         'Indien',
+        //         'Polarbären',
+        //     ],
+        // },
+        // {
+        //     'Warum befindet sich der Zug im Film aus dem Jahr 1964 auf der Insel des fehlerhaften Spielzeugs?': [
+        //         'Viereckige Räder',
+        //         'Keine Dampfmaschine',
+        //         'Farbe stimmt nicht',
+        //         'Pfeift nicht',
+        //     ],
+        // },
+        // {
+        //     'Wie lautet der Name des Schachtelmännchens im Film aus dem Jahr 1964?': [
+        //         'Charlie',
+        //         'Sam',
+        //         'Billy',
+        //         'Jack',
+        //     ],
+        // },
+        // {
+        //     'Warum hat der Weihnachtsmann im Film aus dem Jahr 1964 Weihnachten beinahe abgesagt?': [
+        //         'Sturm',
+        //         'Kein Schnee',
+        //         'Kein Spielzeug',
+        //         'Die Rentiere waren krank',
+        //     ],
+        // },
+        // {
+        //     'Welches tierische Geräusch machte die Elfe im Film aus dem Jahr 1964, um den Bumble abzulenken?': [
+        //         'Oink',
+        //         'Knurr',
+        //         'Wauwau',
+        //         'Miau',
+        //     ],
+        // },
+        // {
+        //     'Wie lautet der Name des Goldsuchers im Film aus dem Jahr 1964?': [
+        //         'Yukon Cornelius',
+        //         'Slider Sam',
+        //         'Bumble',
+        //         'Jack',
+        //     ],
+        // },
+        // {
+        //     'Wie weit ziehen Rentiere auf ihren Wanderungen?': [
+        //         '4800 km',
+        //         '1100 km',
+        //         '800 km',
+        //         '0 km',
+        //     ],
+        // },
+        // {
+        //     'Wie schnell läuft ein Rentier?': [
+        //         '77 km pro Stunde',
+        //         '27 km pro Stunde',
+        //         '30 km pro Stunde',
+        //         '22 km pro Stunde',
+        //         '83 km pro Stunde',
+        //         '65 km pro Stunde',
+        //     ],
+        // },
+    ],
+};
